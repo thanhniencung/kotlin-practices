328dd71f5080461df873a8bb7cf6f07ba34d3c1c
Kien Nguyen <ryan@exgo.vn>
2020-02-20 11:09:48 +0700

    var + val

--- src/Variables.kt
+++ src/Variables.kt
@@ -1,5 +1,24 @@
-import kotlin.reflect.typeOf
-
 fun main() {
+   // Int, Long, Float, String, Hex, Binary, Char
+   val age: Int = 10
+   val myLong: Long = 10L
+   val myFloat: Float = 10.1F
+   val myHex: Int = 0xFF
+   val myBi: Int = 0xb11
+   var ch: Char = 'A'
+   var name: String = "Code4Func"
 
+   // demo String
+   val str1 = "Iam " + "Ryan Nguyen"
+   val mLines = """ 
+      SELECT * 
+      FROM users
+      WHERE 
+         email = 'ryan@gmail.com'
+   """
+   println(mLines)
+
+   val ageString = "iam $age"
+   val ageString2 = "iam ${age + 1}"
+   println(ageString2)
 }
