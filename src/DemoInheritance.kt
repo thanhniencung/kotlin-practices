cd9bd37f6b5974216509edcffba44053f1d8b564
Kien Nguyen <ryan@exgo.vn>
2020-02-23 17:13:11 +0700

    inheritance

---
+++ src/DemoInheritance.kt
@@ -0,0 +1,34 @@
+fun main() {
+    var dog = Dog()
+    dog.talk()
+
+    var cat = Cat("Cat")
+    cat.talk()
+}
+
+open class Animal(var name: String = "") {
+    open fun talk(){}
+}
+
+class Dog : Animal() {
+    init {
+        this.name = "Dog"
+    }
+
+    override fun talk() {
+        println("Gau gau")
+    }
+}
+
+class Cat : Animal {
+    init {
+        this.name = "Cat"
+    }
+    constructor(name: String) : super(name)
+
+    override fun talk() {
+        println("Meo meo")
+    }
+}
+
+
